724ac94a9b0fa28f176fb7179ec74d2d99874fd1
William Petitpierre <william.petitpierre@dailymotion.com>
2026-02-12 16:36:51 +0100

    updated cors

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     SUPABASE_URL: str = os.getenv("SUPABASE_URL", "")
     SUPABASE_PUBLISHABLE_KEY: str = os.getenv("SUPABASE_PUBLISHABLE_KEY", "")
 
-    BACKEND_CORS_ORIGINS: list = ["https://statics.dmcdn.net", "https://localhost:8080"]
+    BACKEND_CORS_ORIGINS: list = ["https://statics.dmcdn.net", "https://localhost:8080", "https://cebed.stg.dm.gg/"]
 
     JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "")
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
